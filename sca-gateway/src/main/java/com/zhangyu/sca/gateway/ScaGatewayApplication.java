30a9960b46c8a8249f025717077ed4ea29704656
zerozayu <304307556@qq.com>
2024-05-17 01:35:35 +0800

    feat: init gateway

---
+++ sca-gateway/src/main/java/com/zhangyu/sca/gateway/ScaGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.zhangyu.sca.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * gateway网关主启动类
+ *
+ * @author zhangyu
+ * @date 2024/5/17 00:58
+ */
+@SpringBootApplication
+public class ScaGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ScaGatewayApplication.class, args);
+    }
+}
